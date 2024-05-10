fd492808c70eae531270dac32c0e2ac97f04c1a5
wangyq <985759262@qq.com>
2024-05-10 19:39:08 +0800

    test: dialogues

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 use std::env;
-use teloxide::{prelude::*, update_listeners::webhooks};
+use teloxide::{ update_listeners::webhooks};
+use teloxide::{dispatching::dialogue::InMemStorage, prelude::*};
+
+type MyDialogue = Dialogue<State, InMemStorage<State>>;
+type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
+
+#[derive(Clone, Default)]
+pub enum State {
+    #[default]
+    Start,
+    ReceiveFullName,
+    ReceiveAge {
+        full_name: String,
+    },
+    ReceiveLocation {
+        full_name: String,
+        age: u8,
+    },
+}
+
 
 #[tokio::main]
 async fn main() {
-    // todo: 在外部声明
-    env::set_var("RUST_LOG", "debug");
     pretty_env_logger::init();
     log::info!("Starting ping-pong bot...");
 
@@ -14,15 +31,75 @@ async fn main() {
     let url = env::var("EUGENE_BOT_DOMAIN").unwrap().parse().unwrap();
     let listener = webhooks::axum(bot.clone(), webhooks::Options::new(addr, url))
         .await
-        .expect("Couldn't setup webhook");
+        .unwrap();
 
-    teloxide::repl_with_listener(
-        bot,
-        |bot: Bot, msg: Message| async move {
-            bot.send_message(msg.chat.id, "pong").await?;
-            Ok(())
-        },
-        listener,
-    )
-        .await;
+    let mut dispatcher = Dispatcher::builder(bot, Update::filter_message()
+        .enter_dialogue::<Message, InMemStorage<State>, State>()
+        .branch(dptree::case![State::Start].endpoint(start))
+        .branch(dptree::case![State::ReceiveFullName].endpoint(receive_full_name))
+        .branch(dptree::case![State::ReceiveAge { full_name }].endpoint(receive_age))
+        .branch(
+            dptree::case![State::ReceiveLocation { full_name, age }].endpoint(receive_location),
+        ),
+    ).build();
+    dispatcher.dispatch_with_listener(listener, teloxide::error_handlers::LoggingErrorHandler::new()).await;
+}
+
+async fn start(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+    bot.send_message(msg.chat.id, "Let's start! What's your full name?").await?;
+    dialogue.update(State::ReceiveFullName).await?;
+    Ok(())
+}
+
+async fn receive_full_name(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+    match msg.text() {
+        Some(text) => {
+            bot.send_message(msg.chat.id, "How old are you?").await?;
+            dialogue.update(State::ReceiveAge { full_name: text.into() }).await?;
+        }
+        None => {
+            bot.send_message(msg.chat.id, "Send me plain text.").await?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn receive_age(
+    bot: Bot,
+    dialogue: MyDialogue,
+    full_name: String, // Available from `State::ReceiveAge`.
+    msg: Message,
+) -> HandlerResult {
+    match msg.text().map(|text| text.parse::<u8>()) {
+        Some(Ok(age)) => {
+            bot.send_message(msg.chat.id, "What's your location?").await?;
+            dialogue.update(State::ReceiveLocation { full_name, age }).await?;
+        }
+        _ => {
+            bot.send_message(msg.chat.id, "Send me a number.").await?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn receive_location(
+    bot: Bot,
+    dialogue: MyDialogue,
+    (full_name, age): (String, u8), // Available from `State::ReceiveLocation`.
+    msg: Message,
+) -> HandlerResult {
+    match msg.text() {
+        Some(location) => {
+            let report = format!("Full name: {full_name}\nAge: {age}\nLocation: {location}");
+            bot.send_message(msg.chat.id, report).await?;
+            dialogue.exit().await?;
+        }
+        None => {
+            bot.send_message(msg.chat.id, "Send me plain text.").await?;
+        }
+    }
+
+    Ok(())
 }
